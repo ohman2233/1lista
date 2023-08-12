3897579f3348de52b49dc69b95f578335d7f6ab1
CHRISTIAN NORONHA PICOLI <christian.picoli@a.ucb.br>
2023-08-12 10:48:03 -0300

    descricao12

--- arquivo.c
+++ arquivo.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
 
 int main(int argc, char* argv[]){
+    int n;
+    printf("digite um numero:\n");
+    scanf("%d",&n);
 
-    printf("ola mundo");
+    if(n%2==0||n%3==0||n%5==0||n%7==0){
+
+        printf("numero nao primo");
+
+    }
+    else{printf("numero primo");}
 
     return 0;
 }
